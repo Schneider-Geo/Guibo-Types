f8ec3fd8c5f344dfbd3aff8c9c2fdf1ff1f19750
guntivikas <guntivikas21@gmail.com>
2023-11-23 16:00:43 +0100

    Adding names property to category

--- src/content/category.ts
+++ src/content/category.ts
@@ -5,11 +5,14 @@ export interface Category {
 	customId?: string;
 	space?: string;
 	name?: string;
+	names?: {
+		[lang: string]: string;
+	};
 	sorting?: number;
 	open?: boolean;
 	visible?: boolean;
 	geocontent? : Array<string>;
   textcontent? : Array<string>;
 
- 
+
  }
