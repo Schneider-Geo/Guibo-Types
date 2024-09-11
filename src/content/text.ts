14f2225aa4a472ae46cda12c7b0135f765317d6b
Jochen Schneider <jochen@schneidergeo.com>
2024-09-11 16:35:25 +0200

    -

--- src/content/text.ts
+++ src/content/text.ts
@@ -3,13 +3,14 @@ export interface Text {
 
 	id?: number;
 	link?: string;
-	
 	bundle?: string;
 	space?: string;
 	category?: string;
 	title?: string;
+	titles?: Record<string, string>;
 	remark?: string;
 	description?: string;
+	descriptions?: Record<string, string>;
 	images?: Array<string>;	
 	sorting: number;
 	context: Context;
